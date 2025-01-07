632153c92fee0693b8caa69265363dce8bb168f4
dani_edo <dani.edogawa.17@gmail.com>
2025-01-08 00:54:36 +0700

    use and dereference pointer

--- pointers.go
+++ pointers.go
@@ -8,10 +8,10 @@ func main() {
 	age := 32 // regular variable
 	fmt.Println("Age:", age)
 
-	adultYears := getAdultYears(age)
+	adultYears := getAdultYears(&age)
 	fmt.Println("Adult years:", adultYears)
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func getAdultYears(age *int) int {
+	return *age - 18
 }
